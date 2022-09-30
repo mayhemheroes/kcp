059dd657fcace1519bb93b23e423c615b37e1584
ebell495 <ericbell495@gmail.com>
2022-09-29 18:11:14 -0700

    Update Fuzz Harness

--- fuzz/fuzz_targets/kcp-fuzz.rs
+++ fuzz/fuzz_targets/kcp-fuzz.rs
@@ -20,23 +20,50 @@ fuzz_target!(|data: &[u8]| {
     if data.len() > 5 {
         let opt = data[0];
         let conv = unsafe{std::ptr::read(&data[1])} as u32;
-        let fuzz_data = &data[5..];
+        let mut fuzz_data = &data[5..];
+        let mut mut_fuzz_data = vec![0 as u8; data.len() - 5];
+        mut_fuzz_data.clone_from_slice(fuzz_data);
+        let mut mut_slice_fuzz_data = &mut mut_fuzz_data[..];
         match opt {
             0=> {
                 let mut kcp= Kcp::new(conv, KcpOutput {} );
-                let _ = kcp.input(fuzz_data);
+                let a = kcp.input(fuzz_data);
+                kcp.update(conv);
+                kcp.flush();
             },
             1=> {
                 let mut kcp= Kcp::new_stream(conv, KcpOutput {} );
-                let _ = kcp.input(fuzz_data);
+                let a = kcp.input(fuzz_data);
+                kcp.update(conv);
+                kcp.flush();
             },
             2=> {
                 let mut kcp= Kcp::new(conv, KcpOutput {} );
-                let _ = kcp.send(fuzz_data);
+                let a = kcp.input(fuzz_data);
+                kcp.move_buf();
+                let b = kcp.recv(&mut mut_slice_fuzz_data);
+                kcp.update(conv);
+                kcp.flush();
             },
             3=> {
                 let mut kcp= Kcp::new_stream(conv, KcpOutput {} );
-                let _ = kcp.send(fuzz_data);
+                let a = kcp.input(fuzz_data);
+                kcp.move_buf();
+                let b = kcp.recv(&mut mut_slice_fuzz_data);
+                kcp.update(conv);
+                kcp.flush();
+            },
+            4=> {
+                let mut kcp= Kcp::new(conv, KcpOutput {} );
+                let a = kcp.send(fuzz_data);
+                kcp.update(conv);
+                kcp.flush();
+            },
+            5=> {
+                let mut kcp= Kcp::new_stream(conv, KcpOutput {} );
+                let a = kcp.send(fuzz_data);
+                kcp.update(conv);
+                kcp.flush();
             },
             _=> ()
         }
